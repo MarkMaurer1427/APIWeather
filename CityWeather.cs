d58623dbd4f36cc49002875270de8c6c773fc5a1
Mark Maurer <markmaurer1427@gmail.com>
2021-05-25 16:59:47 -0500

    Add project files.

---
+++ CityWeather.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace APIWeather
+{
+    class CityWeather
+    {
+        string name;
+        string urlC;
+        string urlF;
+        string tempC;
+        string tempF;
+        string hum;
+
+        public CityWeather(int id)
+        {
+            var client = new HttpClient();
+            urlC = $"https://api.openweathermap.org/data/2.5/weather?id={id}&appid=9319a57f9c0565bf38f245d8a721f21b&units=metric";
+            urlF = $"https://api.openweathermap.org/data/2.5/weather?id={id}&appid=9319a57f9c0565bf38f245d8a721f21b&units=imperial";
+            var pingC = client.GetStringAsync(urlC).Result;
+            var mainC = JObject.Parse(pingC).GetValue("main").ToString();
+            var pingF = client.GetStringAsync(urlF).Result;
+            var mainF = JObject.Parse(pingF).GetValue("main").ToString();
+            name = JObject.Parse(pingC).GetValue("name").ToString();
+            tempC = JObject.Parse(mainC).GetValue("temp").ToString();
+            tempF = JObject.Parse(mainF).GetValue("temp").ToString();
+            hum = JObject.Parse(mainC).GetValue("humidity").ToString();
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"City: {name}");
+            Console.WriteLine($"Temp in Celsius: {tempC}");
+            Console.WriteLine($"Temp in Fahrenheit: {tempF}");
+            Console.WriteLine($"Humidity: {hum}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace APIWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var SATX = new CityWeather(4726206);
+            SATX.PrintInfo();
+
+            Console.WriteLine();
+
+            var Birmingham = new CityWeather(4049979);
+            Birmingham.PrintInfo();
+
+
+
+
+
+
+
+        }
+    }
+}
